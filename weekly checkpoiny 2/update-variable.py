60af5a0cfb604b6a79ee14ce387c87b2f4310a9e
RICARDO77x <archivosinfo77@gmail.com>
2026-08-20 16:56:35 +0000

    tareas

---
+++ weekly checkpoiny 2/update-variable.py
@@ -0,0 +1,37 @@
+def main():
+    #Augmented Assignment Operator
+    money = 5
+    money += 10
+    print(money)# this will print 15
+
+    # Substraction Assignment Operator
+    minutes = 60
+    minutes -= 25
+    print(minutes) # This will print 35
+
+    # Multiplication Assignment Operator
+    skill = 10
+    skill *=20
+    print (skill)
+
+     text = 10
+    text *=20
+    print (text)
+
+    # Division Assignment Operator
+
+    pizzas = 8
+    people = int(input("Number of people at the pizza party:"))
+    pizzas /=people
+    print(pizza)
+
+    #Modulos Assignmet Operator
+    leftover = 8
+    leftover %= people
+    print(leftover)
+
+
+
+
+if __name__== "__main__":
+    main()
