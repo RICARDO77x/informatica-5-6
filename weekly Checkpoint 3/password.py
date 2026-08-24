901300fdaa7361be3597a4ae6e24d88c6a3acb54
RICARDO77x <archivosinfo77@gmail.com>
2026-08-24 19:38:44 +0000

    tareas

---
+++ weekly Checkpoint 3/password.py
@@ -0,0 +1,27 @@
+def main():
+    password = "12345"
+
+    attempt = input ("digite la contraseña.")
+    if attempt == password:
+        print("The password is correct")
+
+print("see you later.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ =="__main__":
+    main()
