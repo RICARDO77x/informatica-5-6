c3c6344b3412f4bcd4af12f49728782de99e24f2
RICARDO77x <archivosinfo77@gmail.com>
2026-08-27 16:37:42 +0000

    tareas

---
+++ Midterm Test (Term 1)/atmosphere.py
@@ -0,0 +1,24 @@
+def main():
+
+    atmospheric= int(input("target atmospheric layer: "))
+    if atmospheric > exosphere:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ =="__main__":
+    main()
--- weekly Checkpoint 3/if-exercises.py
+++ weekly Checkpoint 3/if-exercises.py
@@ -1,12 +1,12 @@
 def main():
-    # Easy Calculator
+
     number1 = int(input("Give me a number: "))
     if number1 >= 0:
         print(number1)
     elif number1 <= 0:
         print(number1 * -1)
-        
-    # Medium Calculator
+
+   
     print("Calculator Medium")
     number2 = int(input("Give me a number: "))
     number3 = int(input("Give me a number: "))
@@ -14,7 +14,7 @@ def main():
     print("2 = Substract")
     print("3 = Multiply")
     print("4 = Divide")
-    
+
     operation = int(input("What operation do you want to perform: "))
     if operation == 1:
         answer = number2 + number3
