fc43ba11205a6f03a34f1f6433a6ca02c2017ceb
RICARDO77x <archivosinfo77@gmail.com>
2026-08-27 08:08:40 -0600

    Actualizar if-exercises.py
    tarea

--- weekly Checkpoint 3/if-exercises.py
+++ weekly Checkpoint 3/if-exercises.py
@@ -1,21 +1,35 @@
 def main():
-
-    calcular = int(input("pon un numero: "))
-    if calcular >= 0:
+    # Easy Calculator
+    number1 = int(input("Give me a number: "))
+    if number1 >= 0:
+        print(number1)
+    elif number1 <= 0:
+        print(number1 * -1)
         
-    print = sum
-    print = substract
-
-
-
-
-
-
-
-
-
-
-
+    # Medium Calculator
+    print("Calculator Medium")
+    number2 = int(input("Give me a number: "))
+    number3 = int(input("Give me a number: "))
+    print("1 = Add")
+    print("2 = Substract")
+    print("3 = Multiply")
+    print("4 = Divide")
+    
+    operation = int(input("What operation do you want to perform: "))
+    if operation == 1:
+        answer = number2 + number3
+        print("Here is your result", answer)
+    elif operation == 2:
+        answer = number2 - number3
+        print("Here is your result", answer)
+    elif operation == 3:
+        answer = number2 * number3
+        print("Here is your result", answer)
+    elif operation == 4:
+        answer = number2 / number3
+        print("Here is your result", answer)
+    else:
+        print("Invalid Operation")
 
 
 
