40f1396088ed53ad6f99870ef53a9cf10bdfb46c
RICARDO77x <archivosinfo77@gmail.com>
2026-09-01 10:16:03 -0600

    Actualizar coinflip.py
    Tarea

--- weekly Checkpoin 4/coinflip.py
+++ weekly Checkpoin 4/coinflip.py
@@ -1,28 +1,18 @@
-import ramdom
 
-def main():
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ =="__main__":
-    main()
+import random
+2
+3 def main():
+4.
+5
+6
+diel = random. randint (1, 6)
+die2 = random. randint (1, 6)
+print (diel +)
+7
+8
+if
+9
+name
+main()
+"
+main_":
