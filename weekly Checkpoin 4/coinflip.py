5f48a6d581c351ad84c99ba742a98f86eb27bf36
RICARDO77x <archivosinfo77@gmail.com>
2026-09-01 10:20:21 -0600

    Actualizar coinflip.py
    Tarea

---
+++ weekly Checkpoin 4/coinflip.py
@@ -0,0 +1,13 @@
+import random
+
+def main():
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    print(die1 + die2)
+
+    # figures = ["car", "windmill", "camel", "tiki", "sumo"]
+    # random.shuffle(figures)
+    # print(figures)
+
+if __name__ == "__main__":
+    main()
