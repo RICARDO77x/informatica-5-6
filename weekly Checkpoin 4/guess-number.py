cd4621bfbb09841ad0fea3cdaf3ec5f2d020d683
RICARDO77x <archivosinfo77@gmail.com>
2026-09-03 17:28:49 +0000

    tareas

---
+++ weekly Checkpoin 4/guess-number.py
@@ -0,0 +1,31 @@
+import random
+
+def main():
+
+    name = input("What's your name? ").title().strip()
+    print(f"Hello {name}!")
+    print("difficulty level")
+    print("easy")
+    print("medium")
+    print("hard")
+    difficulty=input("Choose a difficulty level")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ =="__main__":
+    main()
