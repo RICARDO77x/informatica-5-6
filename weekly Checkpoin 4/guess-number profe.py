8f0472c3bdc5731fdc425b142ab64d923972bf64
RICARDO77x <archivosinfo77@gmail.com>
2026-09-03 20:20:21 +0000

    tareas

---
+++ weekly Checkpoin 4/guess-number profe.py
@@ -0,0 +1,25 @@
+import random
+def main():
+    name = input("Hello! What is your name? ")
+    print(f"Well, {name}, I am thinking of a number between 1 and 100.")
+
+    # Start the game with random number
+    number = random.randint(1, 100)
+    guess = 0
+
+    while guess != number:
+        guess = int(input("take a guess: "))
+        if guess> number:
+            print("Your guess is too high")
+        elif guess < number:
+            print("Your guess is too low")
+
+    print(f"Good Job, {name}! You guesse my number!")
+
+
+
+
+
+
+if name == " main ":
+    main()
