4c4c1cd2b6c78aa476f35625ae9863496437fff1
RICARDO77x <archivosinfo77@gmail.com>
2026-09-09 14:14:15 -0600

    Actualizar HYDRATION-PROGRAM.py

--- weekly Checkpoin 4/HYDRATION-PROGRAM.py
+++ weekly Checkpoin 4/HYDRATION-PROGRAM.py
@@ -1,21 +1,26 @@
 import time
+
 def main():
-    start = 100
+    start = 160
 
-    name = input("Hi! What's your name?: "). title().strip()
-    print (f"HI (name)! The football game is about to start, I'll remember you when to drink waer))
+    name = input("Hi! What's your name? ").title().strip()
+    print(f"HI {name}! The football game is about to start, I'll remember you when to drink water.")
     ready = input("Are you ready to start?:")
-
-
+    print("the time is estart")
+    print()
+    print()
+    print("-----------------------------------------------------------------------------------------")
 
     while start > 0:
-        if ready = "yes":
-            print(water pause has started.")
-            time sleep(125)
-            start-=5
-        elif ready = "no":
-        print ("okay, I'll wait")
-        break
+        if ready == "yes":
+            print("Water pause has started.")
+            time.sleep(60)
+            start -= 1
+        elif ready == "no":
+            print("Okay, I'll wait")
+            break
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
+
+
