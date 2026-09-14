3b1f3ff5ce332f17274f3ef7fcf94ee79c1ba8fe
RICARDO77x <archivosinfo77@gmail.com>
2026-09-14 19:29:19 +0000

    tarea

---
+++ term review/weekly-Checkpoin 5/radio-playlist.py
@@ -0,0 +1,17 @@
+def main():
+    playlist = ["Boston", "Dracula", "I Knew It, I Knew You", "hate that i made you love me", "Risk It All"]
+    playlist.append("Be By You")
+    print(playlist)
+    playlist.insert(0, "Bohemian Rhapsody")
+    print(playlist)
+    playlist.pop(4)
+    print(playlist)
+    print(playlist.index("Risk It All"))
+    print("Number of songs in the playlist:", len(playlist))
+    playlist.reverse()#
+    print(playlist)
+    playlist.sort()
+    print(playlist)
+
+if __name__ == "__main__":
+    main()
