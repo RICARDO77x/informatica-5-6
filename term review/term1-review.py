dce091bb030cbc8f7587ed40d2225765cd8575ea
RICARDO77x <archivosinfo77@gmail.com>
2026-09-14 13:50:30 -0600

    Actualizar term1-review.py

--- term review/term1-review.py
+++ term review/term1-review.py
@@ -1,24 +1,20 @@
 from datetime import datetime
+
 def main():
-    day = datetime.now().weekday()
-    days=[Monda]
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     print("What day is it today?")
     day = int(input())
+    
     if day < 4:
-       print("It's a weekday")
-       remaining = 5 - day
-       print(remaining, "days until the weekend")
+        print("It's a weekday")
+        remaining = 5 - day
+        print(remaining, "days until the weekend")
     elif day == 4:
-        print("It´s Friday")
+        print("It's Friday")
         print("Just a day left until the weekend")
     else:
         print("It's the weekend!")
 
-
-
-
-
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
+
