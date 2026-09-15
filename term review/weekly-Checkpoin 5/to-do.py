a59fc3654cb91d4c2e2d71f67ecb74da693b02cd
RICARDO77x <archivosinfo77@gmail.com>
2026-09-15 17:30:19 +0000

    tareas

---
+++ term review/weekly-Checkpoin 5/to-do.py
@@ -0,0 +1,29 @@
+def main():
+    tareas = []
+    while True:
+        print("Hello, I am your listing assistant; I am here to note down whatever you tell me.")
+        print(F"you have {len(tareas)} tareas to do")
+        print(tareas)
+        command = input("what do you want to do? (add, complete, or stop): ").lower()
+        if command == "add":
+            new_tareas = input("Enter a new tareas: ")
+            tareas.append(new_tareas)
+        elif command == "stop":
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
