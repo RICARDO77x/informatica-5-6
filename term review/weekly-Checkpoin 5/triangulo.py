8891cf2c950d3a0eb5d1b398c529ee61ce9d2c4b
RICARDO77x <archivosinfo77@gmail.com>
2026-09-17 20:20:41 +0000

    chamba

---
+++ term review/weekly-Checkpoin 5/triangulo.py
@@ -0,0 +1,49 @@
+def main():
+    triangle = ["."]
+
+    height=int(input("height?"))
+
+    for base in range(height):
+        print(base)
+        print("*" *(base+1))
+        print("*" *(base+2))
+
+        print("*" *(base+3))
+        print("*" *(base+4))
+
+        print("*" *(base+5))
+        print("*" *(base+6))
+
+        print("*" *(base+7))
+        print("*" *(base+8))
+
+        print("*" *(base+9))
+        print("*" *(base+10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
