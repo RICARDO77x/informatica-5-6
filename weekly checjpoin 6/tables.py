fea2c2bef001746c8d28b94f21042acd8169f6c4
RICARDO77x <archivosinfo77@gmail.com>
2026-09-23 20:20:35 +0000

    tareas

---
+++ weekly checjpoin 6/tables.py
@@ -0,0 +1,31 @@
+def main():
+    tables = ["1", "2", "3", "4", "5", "6",  "7", "8", "9",  "10"]
+    print("Which number do you want to study?")
+    numbers = int(input("number to 1 to 10 "))
+    for multiplication in range (numbers):
+        print(multiplication)
+        print("*" *(numbers+1))
+        print("*" *(numbers+2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ =="__main__":
+    main()
